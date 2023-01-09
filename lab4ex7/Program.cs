05f11a26f2e1ff11decde32cc3bd4e227e3abdee
madalina.toma <madalina.toma@softem.ro>
2023-01-09 23:00:08 +0200

    Add project files.

---
+++ lab4ex7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab4ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 7 dificultate ridicata
+            Scrieti o functie recursiva care pentru un numar “n” primit ca parametru, va
+            afisa urmatoarea piramida a numerelor:
+             */
+            Console.WriteLine("Piramida");
+            GetPiramida(8,8);
+        }
+
+        static int GetPiramida(int n, int final)
+        {
+            n = n - 1;
+            if (n == 0)
+            {
+                return n;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{ n }");
+            }
+           
+            Console.WriteLine();
+            return GetPiramida(n,final);
+        }
+    }
+}
